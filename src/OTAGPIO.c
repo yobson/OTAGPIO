7263a5d5776901fa73d4e689f843c385dbd7806d
yobson <jam.hobson@hotmail.co.uk>
2016-12-30 16:21:39 +0000

    Added action index as param to /udaction and implemented and programed the digital read and write functions

--- src/OTAGPIO.c
+++ src/OTAGPIO.c
@@ -25,6 +25,11 @@ int setPinMode(struct http_request *req)
 	http_argument_get_int32(req, "num", &pinNumber);
 	http_argument_get_string(req, "mode", &mode);
 
+	if (strncmp(mode, "READ", 4)) {
+		pinMode(pinNumber, OUTPUT);
+	} else {
+		pinMode(pinNumber, INPUT);
+	}
 
 	http_response(req, 200, NULL, 0);
 	return KORE_RESULT_OK;
@@ -32,17 +37,39 @@ int setPinMode(struct http_request *req)
 
 int pinWrite(struct http_request *req)
 {
+	http_populate_get(req);
+	int pinNumber, state;
+	http_argument_get_int32(req, "num", &pinNumber);
+	http_argument_get_int32(req, "state", &state);
+
+	digitalWrite(pinNumber, (state == 1) ? HIGH : LOW);
+
 	http_response(req, 200, NULL, 0);
 	return KORE_RESULT_OK;
 }
 
 int pinRead(struct http_request *req) {
-	http_response(req, 200, NULL, 0);
+	http_populate_get(req);
+	int pinNumber, state;
+	http_argument_get_int32(req, "num", &pinNumber);
+
+	state = digitalRead(pinNumber);
+
+	http_response(req, 200, (void *) &state, sizeof(int));
 	return KORE_RESULT_OK;
 }
 
 int udaction(struct http_request *req) 
 {
-	http_response(req, 200, NULL, 0);
+	http_populate_get(req);
+	Data data, ret;
+	int act;
+	http_argument_get_string(req, "num", (char *)data.data);
+	http_argument_get_int32(req, "length", &data.dataSize);
+	http_argument_get_int32(req, "action", &act);
+
+	ret = actions[act](data);
+
+	http_response(req, 200, ret.data, ret.dataSize);
 	return KORE_RESULT_OK;
 }
